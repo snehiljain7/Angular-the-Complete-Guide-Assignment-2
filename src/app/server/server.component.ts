4914b0cd517c1818f9909fa6735288c63a1cc98b
unknown <snehiljain7@gmail.com>
2021-01-22 16:54:46 +0530

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  username = '';
+
+  onCreateUsername(){
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+  onReset(){
+    this.username = '';
+  }
+
+}
